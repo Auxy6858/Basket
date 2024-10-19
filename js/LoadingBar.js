1d547184f8ccdb63097e104e19952f4da949e0aa
Auxy6858 <Auxy6858@users.noreply.github.com>
2024-10-19 22:29:35 +0000

    deploy: ca3c3052fb61b7da8f82fe7e518fe5446c308d03

---
+++ js/LoadingBar.js
@@ -0,0 +1,14 @@
+﻿window.onload = () => {
+    const loadingBar = document.getElementById("loading-bar");
+    let width = 0;
+    
+    const loadingInterval = setInterval(() => {
+        if (width >= 100){
+            clearInterval(loadingInterval);
+            document.getElementById("loading-bar-container").style.display = "none";
+        } else {
+            width--;
+            loadingBar.style.width = width + "%";
+        }
+    }, 50);
+};
